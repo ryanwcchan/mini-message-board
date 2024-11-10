51e41d1498d0c0474132c490106ac15d600da521
Ryan Chan <122183743+TwelveStacks@users.noreply.github.com>
2024-11-10 15:57:15 -0500

    Create navbar and head partials Add stylesheet to views Create form for creating messages

--- server.js
+++ server.js
@@ -23,18 +23,65 @@
 // })
 
 const express = require('express')
+const path = require('path')
+const browserSync = require('browser-sync').create()
+
 const server = express()
 const PORT = 3000
+
+server.use(express.static(path.join(__dirname, 'public')))
 
 server.set('view engine', 'ejs')
 
 // server.use(express.urlencoded({ extended: true }))
 
+server.use((req, res, next) => {
+    console.log('new request made:')
+    console.log('hostname: ', req.hostname)
+    console.log('path: ', req.path)
+    console.log('method: ', req.method)
+    next();
+})
+
 // Index route
 server.get('/', (req, res) => {
-    res.render('index')
+    const messages = [
+        {
+            text: "Hi there!",
+            user: "Amando",
+            added: new Date()
+        },
+        {
+            text: "Hello World!",
+            user: "Charles",
+            added: new Date()
+        }
+    ];
+    res.render('index', { title: 'Home', messages })
+    // res.sendFile('./views/index.ejs', { root: __dirname })
+})
+
+server.get('/create-message', (req, res) => {
+    res.render('create')
+})
+
+// redirects
+// server.get('/redirect-1', (req, res) => {
+//     res.redirect('/redirect-2')
+// })
+
+// 404 page
+server.use((req, res) => {
+    res.status(404).render('404', { root: __dirname })
 })
 
 server.listen(PORT, () => {
     console.log(`Server is runnong on port ${PORT}`)
+})
+
+browserSync.init({
+    proxy: `http://localhost:${PORT}`,
+    files: ['./views/**/*.ejs', './public/**/*'],
+    open: false,
+    notify: false,
 })
